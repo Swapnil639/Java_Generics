71c7f7983b943d2d0d4fc6f71109e2b4f342d2ae
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-06 10:49:34 +0530

    Added :Give three String find maximum

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -1,10 +1,8 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
-
 public class MaximumNumber {
-    public static Float testCase(Float x, Float y, Float z) {
-        Float max = x;
+    public static String testCase(String x, String y, String z) {
+        String max = x;
         if (y.compareTo(max) > 0 && y.compareTo(z) > 0) {
             max = y;
         } else if (z.compareTo(max) > 0 && z.compareTo(y) > 0) {
@@ -14,11 +12,6 @@ public class MaximumNumber {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter three Float number :");
-        Float x = scanner.nextFloat();
-        Float y = scanner.nextFloat();
-        Float z = scanner.nextFloat();
-        System.out.println("Maximum Of Three Floats is :" + testCase(x, y, z));
+        System.out.println("Maximum Of Three Floats is :" + testCase("Apple","Peach","Banana"));
     }
 }
