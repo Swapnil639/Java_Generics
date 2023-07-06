170d3b028fc124e3fb8013e652fb0994a338c7eb
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-06 10:36:43 +0530

    Added :Give three integer find maximum

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -1,7 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class MaximumNumber {
+    public static Integer testCase(Integer x, Integer y, Integer z) {
+        Integer max = x;
+        if (y.compareTo(max) > 0 && y.compareTo(z) > 0) {
+            max = y;
+        } else if (z.compareTo(max) > 0 && z.compareTo(y) > 0) {
+            max = z;
+        }
+        return max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Java Generics");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter three Integer number :");
+        Integer x = scanner.nextInt();
+        Integer y = scanner.nextInt();
+        Integer z = scanner.nextInt();
+        System.out.println("Maximum Of Three Integers is :" + testCase(x, y, z));
     }
 }
