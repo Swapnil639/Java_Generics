d2c973c4b50090bff4f97122e1504467fa63f790
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-06 10:41:57 +0530

    Added :Give three float find maximum

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -3,8 +3,8 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class MaximumNumber {
-    public static Integer testCase(Integer x, Integer y, Integer z) {
-        Integer max = x;
+    public static Float testCase(Float x, Float y, Float z) {
+        Float max = x;
         if (y.compareTo(max) > 0 && y.compareTo(z) > 0) {
             max = y;
         } else if (z.compareTo(max) > 0 && z.compareTo(y) > 0) {
@@ -15,10 +15,10 @@ public class MaximumNumber {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter three Integer number :");
-        Integer x = scanner.nextInt();
-        Integer y = scanner.nextInt();
-        Integer z = scanner.nextInt();
-        System.out.println("Maximum Of Three Integers is :" + testCase(x, y, z));
+        System.out.println("Enter three Float number :");
+        Float x = scanner.nextFloat();
+        Float y = scanner.nextFloat();
+        Float z = scanner.nextFloat();
+        System.out.println("Maximum Of Three Floats is :" + testCase(x, y, z));
     }
 }
