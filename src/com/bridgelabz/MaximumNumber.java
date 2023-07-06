1db899238917965272c73980e448bb1b02aa12c5
Swapnil639 <swapnilpatil631997@gmail.com>
2023-07-06 10:58:28 +0530

    Refactor:All three method to one generics

--- src/com/bridgelabz/MaximumNumber.java
+++ src/com/bridgelabz/MaximumNumber.java
@@ -1,8 +1,8 @@
 package com.bridgelabz;
 
 public class MaximumNumber {
-    public static String testCase(String x, String y, String z) {
-        String max = x;
+    public static <T extends Comparable> T testCase(T x, T y, T z) {
+        T max = x;
         if (y.compareTo(max) > 0 && y.compareTo(z) > 0) {
             max = y;
         } else if (z.compareTo(max) > 0 && z.compareTo(y) > 0) {
@@ -12,6 +12,8 @@ public class MaximumNumber {
     }
 
     public static void main(String[] args) {
-        System.out.println("Maximum Of Three Floats is :" + testCase("Apple","Peach","Banana"));
+        System.out.println("Maximum among three integer values is :" + testCase(900, 5000, 10));
+        System.out.println("Maximum among three float values is :" + testCase(9.8f, 5.7f, 101.7f));
+        System.out.println("Maximum among three string values is :" + testCase("Swapnil", "Shubham", "Kunal"));
     }
 }
